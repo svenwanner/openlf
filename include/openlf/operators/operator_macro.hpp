2ebebcaee4a77fd1f99b7a54298fdef449cf1f02
Hendrik Siedelmann <hendrik.siedelmann@googlemail.com>
2015-09-22 13:14:48 +0200

    macro

---
+++ include/openlf/operators/operator_macro.hpp
@@ -0,0 +1,86 @@
+/*
+* Copyright (c) 2015 Heidelberg Collaboratory for Image Processing
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of
+* this software and associated documentation files (the "Software"), to deal in
+* the Software without restriction, including without limitation the rights to use,
+* copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+* Software, and to permit persons to whom the Software is furnished to do so,
+* subject to the following conditions:
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+* PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+* OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*
+* Author Sven Wanner, Maximilian Diebold, Hendrick Siedelmann 
+*
+*/
+
+#ifndef _OPENLF_OP_MACRO_H
+#define _OPENLF_OP_MACRO_H
+
+#include "clif/clif_vigra.hpp"
+#include "clif/flexmav.hpp"
+#include "operator_macro.hpp"
+
+#define OPENLF_OP_CLASS_HEADER(NAME) \
+    class NAME : public DspComponent { \
+        public: \
+            NAME(); \
+        protected: \
+          virtual void Process_(DspSignalBus& inputs, DspSignalBus& outputs); \
+        private: \
+          clif::FlexMAV<2> _output_image; \
+    };
+    
+
+#define OPENLF_OP_START(NAME) \
+  namespace openlf { namespace components { \
+\
+  using namespace vigra;\
+  using namespace clif;\
+\
+  namespace { \
+\
+  template<typename T> class NAME##dispatcher {\
+  public:\
+    void operator()(FlexMAV<2> *in_mav, FlexMAV<2> *out_mav)\
+    {\
+      MultiArrayView<2,T> *in = in_mav->template get<T>();\
+      MultiArrayView<2,T> *out = out_mav->template get<T>();
+      
+#define OPENLF_OP_END(NAME) \
+}\
+};\
+\
+}\
+\
+NAME::NAME()\
+{\
+  AddInput_("input");\
+  AddOutput_("output");\
+}\
+\
+void NAME::Process_(DspSignalBus& inputs, DspSignalBus& outputs)\
+{\
+  bool stat;\
+  FlexMAV<2> *in;\
+  \
+  stat = inputs.GetValue(0, in);\
+  assert(stat);\
+  \
+  FlexMAV<2> *out_ptr = &_output_image;\
+  \
+  _output_image.create(in->shape(), in->type());\
+\
+  in->call<NAME##dispatcher>(in, out_ptr);\
+  \
+  stat = outputs.SetValue(0, out_ptr);\
+}\
+}}
+
+#endif
---
+++ src/lib/operators/op_gauss2.cpp
@@ -0,0 +1,36 @@
+/*
+* Copyright (c) 2015 Heidelberg Collaboratory for Image Processing
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of
+* this software and associated documentation files (the "Software"), to deal in
+* the Software without restriction, including without limitation the rights to use,
+* copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+* Software, and to permit persons to whom the Software is furnished to do so,
+* subject to the following conditions:
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+* PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+* OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*
+* Author Sven Wanner, Maximilian Diebold, Hendrick Siedelmann 
+*
+*/
+
+#include "operators.hpp"
+
+
+#include <vigra/convolution.hxx>
+/*#include <vigra/imageinfo.hxx>
+#include <vigra/impex.hxx>
+
+#include "clif/flexmav.hpp"*/
+
+OPENLF_OP_START(OP_Gauss2)
+
+    gaussianSmoothing(*in, *out, 5.0, 5.0);
+    
+OPENLF_OP_END(OP_Gauss2)
