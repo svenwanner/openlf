a63bb706a50f5190452799798d8eecd8f9aace75
Hendrik Siedelmann <hendrik.siedelmann@googlemail.com>
2015-09-25 11:23:45 +0200

    averagechannels

--- src/lib/operators/op_averagechannels.cpp
+++ src/lib/operators/op_averagechannels.cpp
@@ -22,34 +22,50 @@
 
 #include "operators.hpp"
 
-#define NAME OP_AverageChannels
 #define INCOUNT 1
 #define OUTCOUNT 1
 #define INDIM 3
 #define OUTDIM 3
-#define OPENLF_OP_CONSTRUCT_PARAMS 
 
-OPENLF_OP_START(OP_AverageChannels, INCOUNT, OUTCOUNT, INDIM, OUTDIM)
-        
-    if (in[0]->shape()[2] == 1)
+namespace openlf { namespace components { 
+  
+using namespace vigra;
+using namespace clif;
+
+namespace { 
+  
+  template<typename T> class OP_AverageChannels_dispatcher {
+  public:
+    void operator()(OP_AverageChannels *op, FlexMAV<INDIM> **in_mav, FlexMAV<OUTDIM> **out_mav, DspSignalBus *inputs, DspSignalBus *outputs)
+    {
+      MultiArrayView<INDIM, T> *in[INCOUNT]; 
+      MultiArrayView<OUTDIM, T> *out[OUTCOUNT]; 
+      for(int i=0;i<1;i++) 
+        in[i] = in_mav[i]->template get<T>();  
+      for(int i=0;i<1;i++) 
+        out[i] = out_mav[i]->template get<T>(); 
+      
+      
+      if (in[0]->shape()[2] == 1)
         *out[0] = *in[0];
-    else {
+      else {
         vigra::MultiArrayView<2, T> r_in;
         r_in = in[0]->bindAt(2, 0);
         vigra::MultiArrayView<2, T> g_in;
         g_in = in[0]->bindAt(2, 1);
         vigra::MultiArrayView<2, T> b_in;
         b_in = in[0]->bindAt(2, 2);
-
+        
         for (int i=0; i<in[0]->shape()[0]*in[0]->shape()[1]; i++)
-            out[0]->data()[i] = (r_in.data()[i]+g_in.data()[i]+b_in.data()[i])/3.0f;
-    }
-
-};
-
+          out[0]->data()[i] = (r_in.data()[i]+g_in.data()[i]+b_in.data()[i])/3.0f;
+      }
+      
+    };
+    
+  };
 }
 
-NAME::NAME()/*
+OP_AverageChannels::OP_AverageChannels()
 {
   char buf[64]; 
   for(int i=0;i<INCOUNT;i++) { 
@@ -59,11 +75,10 @@ NAME::NAME()/*
   for(int i=0;i<OUTCOUNT;i++) { 
     sprintf(buf, "output_%d", i); 
     AddOutput_(buf); 
-  } 
-  OPENLF_OP_CONSTRUCT_PARAMS 
+  }
 }
 
-void NAME::Process_(DspSignalBus& inputs, DspSignalBus& outputs)
+void OP_AverageChannels::Process_(DspSignalBus& inputs, DspSignalBus& outputs)
 {
   bool stat;
   FlexMAV<INDIM> *in[INCOUNT];
@@ -71,7 +86,7 @@ void NAME::Process_(DspSignalBus& inputs, DspSignalBus& outputs)
   for(int i=0;i<INCOUNT;i++) { 
     stat = inputs.GetValue(i, in[i]); 
     if (!stat) { 
-      printf("OP_AverageChannels : input %d not found - possible type mismatch?", i); 
+      printf("OP_AverageChannels : input %d not found - possible type mismatch?\n", i); 
       abort(); 
     } 
   } 
@@ -80,22 +95,24 @@ void NAME::Process_(DspSignalBus& inputs, DspSignalBus& outputs)
   for(int i=0;i<OUTCOUNT;i++) { 
     out_ptr[i] = &_output_image[i]; 
     _output_image[i].create(Shape3(in[0]->shape()[0], in[0]->shape()[1], 1), in[0]->type());
-  } 
-
-  in[0]->call<OP_AverageChannelsdispatcher>(this, in, out_ptr, &inputs, &outputs);
+  }
+  
+  in[0]->call<OP_AverageChannels_dispatcher>(this, in, out_ptr, &inputs, &outputs);
   
   for(int i=0;i<OUTCOUNT;i++) { 
     stat = outputs.SetValue(i, out_ptr[i]);
     if (!stat) { 
-      printf("OP_AverageChannels: output %d set failed", i); 
+      printf("OP_AverageChannels: output %d set failed\n", i); 
       abort(); 
     } 
   } 
 }
-bool NAME::ParameterUpdating_ (int i, DspParameter const &p) 
+bool OP_AverageChannels::ParameterUpdating_ (int i, DspParameter const &p) 
 { 
   SetParameter_(i, p); 
   return true;
 }
-}}*/
-     
+
+}} // namespace openlf::components
+  
+  
