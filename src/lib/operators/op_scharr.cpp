792626f52ae4b8130063a9079d17bd14a3ec1bda
Hendrik Siedelmann <hendrik.siedelmann@googlemail.com>
2015-09-25 16:41:23 +0200

    opencv's scharr is 20x times faster than the previous vigra code

--- src/lib/operators/op_scharr.cpp
+++ src/lib/operators/op_scharr.cpp
@@ -24,21 +24,39 @@
 #include <vigra/multi_convolution.hxx>
 #include "operators.hpp"
 
+#include "opencv2/imgproc/imgproc.hpp"
+
+using namespace cv;
+
 #define OPENLF_OP_CONSTRUCT_PARAMS \
 
 OPENLF_OP_START_T(OP_Scharr, 1, 2, 3, 3, float)
         
-    Kernel1D<float> D;
-    D.initExplicitly(-1,1) = -1.0/2.0, 0.0, 1.0/2.0;
-    D.setBorderTreatment(BORDER_TREATMENT_REFLECT);
-    Kernel1D<float> S;
-    S.initExplicitly(-1,1) = 3.0/16.0, 10.0/16.0, 3.0/16.0;
-    S.setBorderTreatment(BORDER_TREATMENT_REFLECT);
-                
-    convolveMultiArrayOneDimension(*in[0], *out[0], 1, D);
-    convolveMultiArrayOneDimension(*out[0], *out[0], 0, S);
+  /*Kernel1D<float> D;
+  D.initExplicitly(-1,1) = -1.0/2.0, 0.0, 1.0/2.0;
+  D.setBorderTreatment(BORDER_TREATMENT_REFLECT);
+  Kernel1D<float> S;
+  S.initExplicitly(-1,1) = 3.0/16.0, 10.0/16.0, 3.0/16.0;
+  S.setBorderTreatment(BORDER_TREATMENT_REFLECT);
+              
+  convolveMultiArrayOneDimension(*in[0], *out[0], 1, D);
+  convolveMultiArrayOneDimension(*out[0], *out[0], 0, S);
+  
+  convolveMultiArrayOneDimension(*in[0], *out[1], 0, D);
+  convolveMultiArrayOneDimension(*out[1], *out[1], 1, S);*/
     
-    convolveMultiArrayOneDimension(*in[0], *out[1], 0, D);
-    convolveMultiArrayOneDimension(*out[1], *out[1], 1, S);
+  //this is 21x times faster than above vigra code
+  for (int i=0; i < in[0]->shape()[2]; ++i) {
+    vigra::MultiArrayView<2,T> ch_in = in[0]->bindAt(2, i);
+    //FIXME X should come first!
+    vigra::MultiArrayView<2,float> ch_out_x = out[1]->bindAt(2, i);
+    vigra::MultiArrayView<2,float> ch_out_y = out[0]->bindAt(2, i);
+    Mat cv_in(Size(ch_in.shape(0), ch_in.shape(1)), BaseType2CvDepth(in_mav[0]->type()), ch_in.data());
+    Mat cv_out_x(Size(ch_out_x.shape(0), ch_out_x.shape(1)), BaseType2CvDepth(out_mav[0]->type()), ch_out_x.data());
+    Mat cv_out_y(Size(ch_out_y.shape(0), ch_out_y.shape(1)), BaseType2CvDepth(out_mav[0]->type()), ch_out_y.data());
+    
+    Scharr(cv_in, cv_out_x, CV_32F, 1, 0);
+    Scharr(cv_in, cv_out_y, CV_32F, 0, 1);
+  }
     
 OPENLF_OP_END_T(OP_Scharr, 1, 2, 3, 3, BaseType::FLOAT)
