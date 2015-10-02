95bc754a1b32adc20fea3daae6c6d9db994c3f8a
Hendrik Siedelmann <hendrik.siedelmann@googlemail.com>
2015-10-02 17:26:11 +0200

    add internal dspatch

---
+++ external/dspatch/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_ddc79.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_ddc79.dir/testCCompiler.c.o"
+  "cmTC_ddc79.pdb"
+  "cmTC_ddc79"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_ddc79.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
