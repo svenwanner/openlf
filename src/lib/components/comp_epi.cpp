b65b39c315f4c382c775690131f923e4596a28be
Hendrik Siedelmann <hsiedelm@eule.hci.uni-heidelberg.de>
2015-10-12 17:36:33 +0200

    allow cmake 2.8, process full epi space

--- src/lib/components/comp_epi.cpp
+++ src/lib/components/comp_epi.cpp
@@ -273,7 +273,7 @@ void COMP_Epi::Process_(DspSignalBus& inputs, DspSignalBus& outputs)
   FlexMAV<3> *sink_mav;
       
   #pragma omp parallel for private(sink_mav)
-  for(int i=380;i<450/*subset.EPICount()*/;i++) {
+  for(int i=0;i<subset.EPICount();i++) {
     if (i % 10 == 0)
       printf("proc epi %d\n", i);
     
