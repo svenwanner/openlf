941ff3520f1ae2d6b547c4f0015a0170bf4de6f5
Maximilian Diebold <max.diebold@gmx.com>
2015-10-21 10:19:03 +0200

    put circuit viewer in own file and add load buttion image

---
+++ gui/external/qnodeseditor/include/circuitviewer.h
@@ -0,0 +1,256 @@
+/* Copyright (c) 2012, STANISLAW ADASZEWSKI
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of STANISLAW ADASZEWSKI nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL STANISLAW ADASZEWSKI BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+#ifndef QNEMAINWINDOW_H
+#define QNEMAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtWidgets>
+#include <QMdiArea>
+#include <QSpinBox>
+#include <QDockWidget>
+
+#include <QTextEdit>
+#include <QMainWindow>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QApplication>
+#include <QtGui>
+#include <QScrollBar>
+#include <QScrollArea>
+#include <QLabel>
+#include <QAction>
+#include <QToolBar>
+#include <QMenu>
+#include <QMenuBar>
+#include <QLabel>
+#include <QToolBar>
+#include <QVBoxLayout>
+#include <QLineEdit>
+#include "qnodesthreads.h"
+
+#include "qnesettings.h"
+#include "qneblock.h"
+
+#include "DspCircuit.h"
+
+class QNodesEditor;
+class Circuit_Viewer;
+
+class QNEMainWindow : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	explicit QNEMainWindow(QWidget *parent = 0);
+	~QNEMainWindow();
+
+private slots:
+	void saveFile();
+	void loadFile();
+	void new_circuit_viewer();
+	void onApplicationFocusChanged();
+	void createDockWindows();
+	void addComponent(QListWidgetItem *it);
+	void showCompSettings(DspComponent *comp);
+	void showCompSettings(QNEBlock *comp);
+	void activate(QWidget* wid);
+        void newCircuit(DspCircuit* c);
+        void circuitNameChanged(QString name);
+        
+signals:
+	void itemDoubleClicked();
+
+private:
+
+	QMdiArea *mdiArea = std::nullptr_t();
+
+	QMenu *fileMenu = std::nullptr_t();
+	QMenu *helpMenu = std::nullptr_t();
+	QMenu *viewMenu = std::nullptr_t();
+
+	QToolBar *fileToolBar = std::nullptr_t();
+
+	void createActions();
+	void createMenus();
+	void createToolBars();
+	void createStatusBar();
+	
+	std::vector<QDockWidget*> docks;
+
+	QNodesEditor *nodesEditor = std::nullptr_t();
+	QGraphicsView *view = std::nullptr_t();
+	QGraphicsScene *scene = std::nullptr_t();
+
+	QAction *quitAct = std::nullptr_t();
+	QAction *loadAct = std::nullptr_t();
+	QAction *saveAct = std::nullptr_t();
+	QAction *newAct = std::nullptr_t();
+
+	QListWidget *List1 = std::nullptr_t();
+	QListWidget *List2 = std::nullptr_t();
+
+	QDockWidget *dock = std::nullptr_t();
+	QDockWidget *dock2 = std::nullptr_t();
+        QDockWidget *_circuit_dock = NULL;
+        
+        QLineEdit *_c_name_ed = NULL;
+        
+    Circuit_Viewer *_circuitViewer = NULL;
+    QNESettings *_settings = NULL;
+
+	std::vector<QThread*> threads;
+        std::vector<DspCircuit*> _circuits;
+};
+
+#endif // QNEMAINWINDOW_H
+
+
+
+
+
+
+
+
+
+
+class Circuit_Viewer : public QMainWindow
+
+{
+	Q_OBJECT
+
+public:
+	Circuit_Viewer(QMdiArea *mdiArea = 0, QMainWindow *parent = 0);
+	~Circuit_Viewer();
+
+     void addComponent(DspComponent *comp, bool gui_only = false);
+     void addInputComponent(int pads = 1);
+     void addOutputComponent(int pads = 1);
+     DspCircuit *circuit();
+        
+        
+	//void open(const QString &title);
+	//void setImage(const QString &title, QPixmap *pxmap);
+	//void enableEPI(bool enable);
+
+	//QString userFriendlyCurrentFile();
+	//QString currentFile()  { return curFile; }
+
+	//QScrollArea *scrollArea;
+
+signals:
+  void compSelected(DspComponent *comp);
+  void compSelected(QNEBlock *block);
+
+  void focusChanged();
+
+  void activated(QWidget* w);
+  void newCircuit(DspCircuit* c);
+
+protected:
+	bool event(QEvent* e);
+
+
+private slots:
+	
+	void save();
+	void saveAs();
+    void load();
+	void zoomIn();
+	void zoomOut();
+	void fitToWindow();
+        void onCompSelected(DspComponent *comp);
+        void onCompSelected(QNEBlock *block);
+	void on_action_Pop_Out_triggered();
+	void on_action_Pop_In_triggered();
+	void tick();
+
+
+private:
+	int event_memory;
+
+	QMdiArea *mdiArea;
+	QWidget *popInpopOutWidget;
+
+	// member variable to store click position
+	QPoint m_lastPoint;
+	// member variable - flag of click beginning
+	bool m_mouseClick;
+
+	void scaleImage(double factor);
+	void adjustScrollBar(QScrollBar *scrollBar, double factor);
+	void createToolbar();
+	void createActions();
+	void createMenus();
+        void check_add_circuit();
+
+	QAction *MousePosition;
+	QAction *saveAct;
+	QAction *saveAsAct;
+	QAction *loadAct;
+	QAction *exitAct;
+	QAction *infoAct;
+	QAction *zoomInAct;
+	QAction *zoomOutAct;
+	QAction *fitToWindowAct;
+	QAction *popInAct;
+	QAction *popOutAct;
+	QAction *tickAct;
+
+	const int ToolBar_Height = 40;
+	QToolBar *ToolBar = std::nullptr_t();
+	QMenu *circuitMenu = std::nullptr_t();
+
+
+	
+	//QVBoxLayout* vbox;
+
+	//QMenuBar *menuBar;
+	//QMainWindow *inner;
+	//QMenu *viewMenu;
+
+	//QToolBar *fileToolBar;
+
+
+
+	//double scaleFactor;
+
+	//void setCurrentFile(const QString &fileName);
+	//QString strippedName(const QString &fullFileName);
+
+	//QString curFile;
+	//bool isUntitled;
+        
+        DspCircuit *_circuit;
+        QGraphicsScene *_scene;
+        QGraphicsView *_view;
+        QNodesEditor *_editor;
+        std::vector<QNEBlock*> _blocks;
+        
+        QNEBlock *_input_block = NULL;
+        QNEBlock *_output_block = NULL;
+        
+        bool _extra_window = false;
+};
