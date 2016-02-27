6f9523325104567f77664c781c68970dfd6f7e3c
Hendrik Siedelmann <hendrik.siedelmann@googlemail.com>
2016-02-27 20:18:22 +0100

    fix StopLine check

--- components/comp_epi3d.cpp
+++ components/comp_epi3d.cpp
@@ -578,7 +578,7 @@ void COMP_Epi::Process_(DspSignalBus& inputs, DspSignalBus& outputs)
   get_int_param(this, stop_line, P_IDX::StopLine);
   
   errorCond(start_line >= 0, "StartLine invalid value (%d < 0)!", stop_line, subset.EPICount()); RETURN_ON_ERROR
-  errorCond(stop_line < subset.EPICount(), "StopLine invalid value (%d >= %d)!", stop_line, subset.EPICount()); RETURN_ON_ERROR
+  errorCond(stop_line <= subset.EPICount(), "StopLine invalid value (%d > %d)!", stop_line, subset.EPICount()); RETURN_ON_ERROR
   
   //setup circuit and threading
 #ifdef OPENLF_WITH_OPENMP
